d671c5e709d0a2506b308caa2ad473dd9375ca8b
Wieslaw Samushonga <72025253+JxstWieslaw@users.noreply.github.com>
2023-09-25 14:53:30 +0530

    Initial commit

---
+++ src/Ground.js
@@ -0,0 +1,24 @@
+import * as THREE from "three"
+import { useTexture } from "@react-three/drei"
+import { CuboidCollider, RigidBody } from "@react-three/rapier"
+import grass from "./assets/grass.jpg"
+
+export function Ground(props) {
+  const texture = useTexture(grass)
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+
+  const animateCamera = (e) => {
+    e.stopPropagation()
+    console.log(e)
+  }
+
+  return (
+    <RigidBody {...props} type="fixed" colliders={false}>
+      <mesh onDoubleClick={(e) => animateCamera(e)} receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+      </mesh>
+      <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
+    </RigidBody>
+  )
+}
